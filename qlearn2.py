7b31d8677b4af98213f7b56c7fa414443aae0361
WuTUT <762641171@qq.com>
2019-04-12 20:57:44 +0800

    111

---
+++ qlearn2.py
@@ -0,0 +1,157 @@
+import numpy as np
+import wrsn2 as wr
+from wrsn2 import *
+import sys
+import itertools
+import matplotlib
+from collections import defaultdict
+import plotting
+from plotting import *
+import json
+
+def make_epsilon_greedy_policy(Q, epsilon, nA):
+    """
+    Creates an epsilon-greedy policy based on a given Q-function and epsilon.
+    
+    Args:
+        Q: A dictionary that maps from state -> action-values.
+            Each value is a numpy array of length nA (see below)
+        epsilon: The probability to select a random action . float between 0 and 1.
+        nA: Number of actions in the environment.
+    
+    Returns:
+        A function that takes the observation as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+    
+    """
+    def policy_fn(observation):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        best_action = np.argmax(Q[observation])
+        A[best_action] += (1.0 - epsilon)
+        return A
+    return policy_fn
+
+def q_learning(env, num_episodes, discount_factor=0.99, alpha=0.25, epsilon=0.05):
+    """
+    Args:
+        env: OpenAI environment.
+        num_episodes: Number of episodes to run for.
+        discount_factor: Gamma discount factor.
+        alpha: TD learning rate.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
+    
+    Returns:
+        A tuple (Q, episode_lengths).
+        Q is the optimal action-value function, a dictionary mapping state -> action values.
+        stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+    
+    # The final action-value function.
+    # A nested dictionary that maps state -> (action -> action-value).
+    
+  
+    
+    # with open("nsQ_json.json","r") as json_file:
+    #     nsQ1=json.load(json_file)
+    # json_file.close()
+    # for key in list(nsQ1.keys()):
+    #     nsQ1[tuple(np.array(key.split()))] = np.array(nsQ1[key].split())
+    #     del nsQ1[key]
+
+    # with open("chQ_json.json","r") as json_file:
+    #     chQ1=json.load(json_file)
+    # json_file.close()
+    # for key in list(chQ1.keys()):
+    #     chQ1[tuple(np.array(key.split()))] = np.array(chQ1[key].split())
+    #     del chQ1[key]
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    
+    # for k,v in nsQ1.items():
+    #     nsQ[k]=v
+    # for k,v in chQ1.items():
+    #     chQ[k]=v
+    # for k,v in chQ1.items():
+    #     print(k,v)
+    #     break
+    
+    # Keeps track of useful statistics
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes),
+        episode_transbag=np.zeros(num_episodes))    
+    
+    # The policy we're following
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+    
+
+    for i_episode in range(num_episodes):
+        # Print out which episode we're on, useful for debugging.
+        if (i_episode + 1) % 1 == 0:
+            print("\rEpisode {}/{}.".format(i_episode + 1, num_episodes), end="")
+            sys.stdout.flush()
+        
+        # Reset the environment and pick the first action
+        
+        state=env.reset()
+        
+        state=tuple(state.flatten())
+        # assert isinstance(ns_state,tuple)
+        
+        # One step in the environment
+        
+        for t in itertools.count():
+            #print(ns_state)
+            #print(type(ns_state))
+            # Take a step
+            action_probs = policy(state)
+            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+            next_state, reward, done, data_overflow = env.step(action)
+            
+            next_state=tuple(next_state.flatten())
+            # Update statistics
+          
+            stats.episode_rewards[i_episode] += reward 
+            stats.episode_lengths[i_episode] = t
+            stats.episode_transbag[i_episode]+=data_overflow
+            # TD Update
+            best_next_action = np.argmax(Q[next_state])    
+            td_target = reward + discount_factor * Q[next_state][best_next_action]
+            td_delta = td_target - Q[state][action]
+            Q[state][action] += alpha * td_delta
+                
+            state = next_state
+            
+            
+            if t>200:
+                break
+
+
+
+    
+    return Q, stats
+
+env=wrsn(sensor_node)
+print(((queue_len+1)*(battle_level+1)*distance_level)**sensor_node)
+Q, stats = q_learning(env, 20000)
+for i in range(len(stats.episode_rewards)):
+    stats.episode_rewards[i]/=1
+
+plotting.plot_episode_stats(stats)
+
+ostate=0
+for key in list(Q.keys()):
+    for a in range(env.action_space.n):
+        if np.abs(Q[key][a])<1e-3:
+            ostate+=1
+
+    Q[str(list(key))] = str(Q[key])
+    del Q[key]
+print(ostate)
+print(len(Q)*env.action_space.n)
+print()
+with open("Q_json.json","w") as json_file:
+    json_file.writelines(json.dumps(Q,sort_keys=True, indent=4, separators=(',', ': ')))
+        
+json_file.close()
+
+
---
+++ wrsn2.py
@@ -0,0 +1,82 @@
+import numpy as np
+import gym
+from gym import spaces
+
+#define constant value
+
+queue_len=4
+battle_level=4
+distance_level=2
+sensor_node=5
+
+
+def env_init():
+    
+    B=np.random.randint(0,battle_level+1,sensor_node).reshape(-1,1)
+    D=np.random.randint(0,queue_len+1,sensor_node).reshape(-1,1)
+    H=np.random.randint(0,distance_level,sensor_node).reshape(-1,1)
+    S=np.hstack((B,D,H))
+    
+    #S[sensor_node][0]  B  [1] D [2] H
+
+    data_prob=np.ones(sensor_node)*0.6
+    return S,data_prob
+
+def funEh(distance):
+    Eh=[1,1]
+    return Eh[distance]
+
+def funPh(distance):
+    Ph=[1,1]
+    return Ph[distance]    
+
+def calculate_transprob(S,action,data_prob):
+    data_overflow=0
+    reward=0
+    sensor_id=action//2
+    switch=action%2
+    assert switch==0 or switch ==1
+    if switch==0:
+        S[sensor_id][0]=min(S[sensor_id][0]+funEh(S[sensor_id][2]),battle_level)
+    else:
+        if funPh(S[sensor_id][2])<=S[sensor_id][0]:
+            S[sensor_id][0]=S[sensor_id][0]-funPh(S[sensor_id][2])
+            if S[sensor_id][1]==0:
+                reward=-1
+            else:
+                reward=1
+                S[sensor_id][1]-=1
+        else:
+            reward=-1
+    for i in range(sensor_node):
+        S[i][1]=S[i][1]+1 if data_prob[i]>np.random.uniform(0,1) else S[i][1]
+        if S[i][1]>queue_len:
+            S[i][1]=queue_len
+            data_overflow+=1
+            
+    return S,reward,data_overflow
+
+
+    
+
+class wrsn(gym.Env):
+    def __init__(self,sensor_node):
+        self.action_space = spaces.Discrete(sensor_node*2)
+        self.lowstate=np.tile(np.array([0,0,0]),(sensor_node,1))
+        self.highstate=np.tile(np.array([battle_level-1,queue_len-1,distance_level-1]),(sensor_node,1))
+        
+        self.observation_space = spaces.Box(low=self.lowstate,high=self.highstate,dtype=np.uint8)
+        self.S,self.data_prob=env_init()
+    def step(self,action):
+        done=False
+        reward=0.0
+        self.S,reward,data_overflow=calculate_transprob(self.S,action,self.data_prob)
+        #if data_overflow
+        return self.S,reward,done,data_overflow
+    def _get_obs(self):
+        return self.S
+    def reset(self):
+        self.S,self.data_prob=env_init()
+        return self._get_obs()    
+
+
